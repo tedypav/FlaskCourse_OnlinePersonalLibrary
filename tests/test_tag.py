70a66c1af6a92dece9da6dd54691ba0e65d18b70
unknown <teodorapavlova@yahoo.com>
2022-08-25 08:56:23 +0300

    add one more test for tags

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -3,6 +3,7 @@ from flask_testing import TestCase
 from config import create_app
 from db import db
 from managers.tag import TagManager
+from models import TagModel, resource_tag
 from tests.base import generate_token
 from tests.factories import UserFactory, ResourceFactory
 
@@ -129,6 +130,7 @@ class TestTag(TestCase):
             "resource_id": new_resource2.resource_id,
             "tag": ["test_tag", "test", "tag", "tag", "more_tag"],
         }
+
         resp1 = self.client.post(url, headers=headers1, json=data1)
         assert resp1.status_code == 201
 
@@ -156,3 +158,76 @@ class TestTag(TestCase):
         user2_num_tags_after_delete = len(TagManager.get_tags(user2))
         assert user1_num_tags - 1 == user1_num_tags_after_delete
         assert user2_num_tags == user2_num_tags_after_delete
+
+    def test_create_duplicate_tag(self):
+        """
+        Make sure the user can't register the same tag twice.
+        """
+
+        tested_url = "/tag_resource/"
+
+        user1 = UserFactory()
+        user2 = UserFactory()
+        new_resource1 = ResourceFactory(owner_id=user1.user_id)
+        new_resource2 = ResourceFactory(owner_id=user2.user_id)
+
+        url = "/tag_resource/"
+
+        token1 = generate_token(user1)
+        headers1 = {
+            "Authorization": f"Bearer {token1}",
+            "Content-Type": "application/json",
+        }
+        data1 = {
+            "resource_id": new_resource1.resource_id,
+            "tag": ["test_tag", "test", "tag", "test", "test"],
+        }
+
+        token2 = generate_token(user2)
+        headers2 = {
+            "Authorization": f"Bearer {token2}",
+            "Content-Type": "application/json",
+        }
+        data2 = {
+            "resource_id": new_resource2.resource_id,
+            "tag": ["test_tag", "test", "tag", "tag", "more_tag"],
+        }
+        resp1 = self.client.post(tested_url, headers=headers1, json=data1)
+        assert resp1.status_code == 201
+        assert (
+            len(set(data1["tag"]))
+            == TagModel.query.filter_by(owner_id=user1.user_id).count()
+        )
+
+        for tag in data1["tag"]:
+            tag_id1 = TagModel.query.filter_by(tag=tag, owner_id=user1.user_id).first()
+            db.session.query(resource_tag).filter_by(tag_id=tag_id1.tag_id).count() == 1
+
+        data12 = {
+            "resource_id": new_resource1.resource_id,
+            "tag": ["new_tag", "test_test", "tag", "test"],
+        }
+
+        resp12 = self.client.post(tested_url, headers=headers1, json=data12)
+        assert resp1.status_code == 201
+        assert (
+            len(set(data12["tag"] + data1["tag"]))
+            == TagModel.query.filter_by(owner_id=user1.user_id).count()
+        )
+
+        for tag in data12["tag"]:
+            tag_id12 = TagModel.query.filter_by(tag=tag, owner_id=user1.user_id).first()
+            db.session.query(resource_tag).filter_by(
+                tag_id=tag_id12.tag_id
+            ).count() == 1
+
+        resp2 = self.client.post(tested_url, headers=headers2, json=data2)
+        assert resp2.status_code == 201
+        assert (
+            len(set(data2["tag"]))
+            == TagModel.query.filter_by(owner_id=user2.user_id).count()
+        )
+
+        for tag in data2["tag"]:
+            tag_id2 = TagModel.query.filter_by(tag=tag, owner_id=user2.user_id).first()
+            db.session.query(resource_tag).filter_by(tag_id=tag_id2.tag_id).count() == 1
